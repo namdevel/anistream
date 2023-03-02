f0e895befb3def9521cc2ebfa0d78188414d81e8
sinkaroid <bibsindra@gmail.com>
2023-03-02 23:58:56 +0700

    non logs on prod

--- assets/js/light.js
+++ assets/js/light.js
@@ -1,17 +1,15 @@
 let isLightMode = localStorage.getItem("light-mode");
 if (isLightMode) {
     document.body.classList.add("light-mode");
-    console.log("light-mode");
 } else {
     document.body.classList.remove("light-mode");
-    console.log("dark-mode");
 }
 
 function switchLight() {
     var element = document.body;
     element.classList.toggle("light-mode");
 
-    if (!localStorage.getItem("light-mode")) {
+    if (!isLightMode) {
         localStorage.setItem("light-mode", true);
     } else {
         localStorage.removeItem("light-mode");
