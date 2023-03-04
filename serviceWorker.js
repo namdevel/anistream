67e1f4dd0ca04dcec3de7234c9f439dc5b32c272
sinkaroid <bibsindra@gmail.com>
2023-03-05 05:56:49 +0700

    change this icon because the default one literally crap on mobile

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      return cache.addAll(["./", './assets/img/favicon.png']);
+      return cache.addAll(["./", './assets/img/wibu-makima.png']);
     })
   );
 });
