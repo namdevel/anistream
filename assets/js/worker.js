6769576f17d60f897852907324f0e826997b7b53
sinkaroid <bibsindra@gmail.com>
2023-03-05 04:44:03 +0700

    add worker

---
+++ assets/js/worker.js
@@ -0,0 +1,10 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("serviceWorker.js")
+      .then(r => {
+        console.log(r, "Worker registered");
+      }).catch(e => {
+        console.log(e, "Worker failed to register");
+      });
+  } else {
+    console.log("Error");
+  }
